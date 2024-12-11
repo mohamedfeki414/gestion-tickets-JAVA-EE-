89d8e1515ec76b962eb39cb6063d4b9829ed4c47
feki_mohamed <medfeki33@gmail.com>
2024-12-11 19:56:24 +0100

    ajouter user ressource

--- src/main/java/com/example/support/gestion_tickets/UserResource.java
+++ src/main/java/com/example/support/gestion_tickets/UserResource.java
@@ -1,5 +1,20 @@
 package com.example.support.gestion_tickets;
 
-public class UserResource {
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
+@Path("/users")
+public class UserResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "JohnDoe", "john.doe@example.com"));
+        users.add(new User(2, "JaneSmith", "jane.smith@example.com"));
+        return users;
+    }
 }
+

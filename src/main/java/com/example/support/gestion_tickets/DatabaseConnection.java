0063ffe60759bca1caa5c05418653b0238c4d56c
feki_mohamed <medfeki33@gmail.com>
2024-12-11 19:57:44 +0100

    modifi dbc

--- src/main/java/com/example/support/gestion_tickets/DatabaseConnection.java
+++ src/main/java/com/example/support/gestion_tickets/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/projet_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

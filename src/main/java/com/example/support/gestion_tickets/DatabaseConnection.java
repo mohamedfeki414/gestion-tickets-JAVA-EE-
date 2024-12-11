6fe42e5731181e8ff51897df04811730161936eb
feki_mohamed <medfeki33@gmail.com>
2024-12-11 21:37:43 +0100

    .

--- src/main/java/com/example/support/gestion_tickets/DatabaseConnection.java
+++ src/main/java/com/example/support/gestion_tickets/DatabaseConnection.java
@@ -5,11 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/projet_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "123456";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/projet_db?serverTimezone=UTC"; // Vérifiez l'URL
+        String username = "root"; // Utilisez votre utilisateur MySQL
+        String password = "123456"; // Utilisez votre mot de passe MySQL
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Charger le driver
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection connection = getConnection();
+        if (connection != null) {
+            System.out.println("Connexion réussie !");
+        } else {
+            System.out.println("Échec de la connexion !");
+        }
     }
 }

f2e092203aa129a9c9f67818e881ba1c2ecbc164
feki_mohamed <medfeki33@gmail.com>
2024-12-11 21:19:18 +0100

    AJOUTER CLASSE TESTE POUR TESTER JDBC

---
+++ src/main/java/com/example/support/gestion_tickets/TestDBConnection.java
@@ -0,0 +1,40 @@
+package com.example.support.gestion_tickets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestDBConnection {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // Charger le driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connexion à la base de données
+            String url = "jdbc:mysql://localhost:3306/";
+            String username = "root";
+            String password = "123456";
+            connection = DriverManager.getConnection(url, username, password);
+
+            if (connection != null) {
+                System.out.println("Connexion réussie !");
+            } else {
+                System.out.println("Échec de la connexion !");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
